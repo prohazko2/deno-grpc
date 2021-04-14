8e65b4fe826d15d11cdd86c821b5eb4891fc9441
Oleg Prohazko <olegprohazko@gmail.com>
2021-04-15 01:58:03 +0300

    proto parser

--- proto.ts
+++ proto.ts
@@ -1,29 +1,9 @@
-/*
-> cat rollup.config.js
-
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-
-export default {
-  input: "src/index-minimal.js",
-  output: {
-    file: "protobuf.js",
-    format: "esm",
-  },
-  plugins: [nodeResolve(), commonjs()],
-};
-
- */
-
-import type {
-  //parse,
-  Root,
-  //IParseOptions,
-} from "https://raw.githubusercontent.com/protobufjs/protobuf.js/v6.10.2/index.d.ts";
+import type { Root } from "https://raw.githubusercontent.com/protobufjs/protobuf.js/v6.10.2/index.d.ts";
 
 import protobuf from "./vendor/protobuf@v6.10.2.js";
 
-const xs = protobuf.exports as any;
+const lib = protobuf.exports as any;
 
-console.log(xs);
-console.log(xs.parse);
+export function parse(proto: string): { package: string; root: Root } {
+  return lib.parse(proto);
+}
