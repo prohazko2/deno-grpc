45afd84e8cfd769c272d458fa2c246939d9feb81
Oleg Prohazko <olegprohazko@gmail.com>
2021-04-15 02:46:59 +0300

    add self-signed certs to test http2

--- serve.ts
+++ serve.ts
@@ -1,9 +1,21 @@
 const port = 4500;
 
-console.log(`server gonna serve on port ${port}`);
+const listener = Deno.listenTls({
+  port,
+  certFile: "./cert/cert.pem",
+  keyFile: "./cert/key.pem",
+  alpnProtocols: ["h2", "http/1.1"],
+});
 
-for await (const conn of Deno.listen({ port })) {
-  for await (const { request, respondWith } of Deno.serveHttp(conn)) {
+for await (const conn of listener) {
+  handleConn(conn);
+}
+
+async function handleConn(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const { request, respondWith } of httpConn) {
     respondWith(new Response(`Responding to ${request.url}`));
   }
 }
+
+// > curl -k -vvvvv --http2 https://localhost:4500/xxx
