74f875c9a14a1bb54c2ab81944c63c660bb75445
Oleg Prohazko <olegprohazko@gmail.com>
2021-04-15 01:37:37 +0300

    add protobuf.js dep

---
+++ proto.ts
@@ -0,0 +1,29 @@
+/*
+> cat rollup.config.js
+
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/index-minimal.js",
+  output: {
+    file: "protobuf.js",
+    format: "esm",
+  },
+  plugins: [nodeResolve(), commonjs()],
+};
+
+ */
+
+import type {
+  //parse,
+  Root,
+  //IParseOptions,
+} from "https://raw.githubusercontent.com/protobufjs/protobuf.js/v6.10.2/index.d.ts";
+
+import protobuf from "./vendor/protobuf.js";
+
+const xs = protobuf.exports as any;
+
+console.log(xs);
+console.log(xs.parse);
