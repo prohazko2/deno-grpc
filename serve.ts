e9d7e8f714cafc1ac2696abbe8df7ad1880c49cb
Oleg Prohazko <olegprohazko@gmail.com>
2021-04-16 00:06:19 +0300

    parse req

--- serve.ts
+++ serve.ts
@@ -1,6 +1,15 @@
+import { hexdump } from "https://deno.land/x/prohazko@1.3.3/hex.ts";
+import { parse } from "./proto.ts";
+
 const port = 15070;
 
-import { hexdump } from "https://deno.land/x/prohazko@1.3.3/hex.ts";
+const text = await Deno.readTextFile("./_/test.proto");
+console.log(text);
+
+const { root } = parse(text);
+
+const Req = root.lookupType("HelloRequest");
+const Res = root.lookupType("HelloReply");
 
 console.log(`gonna listen on ${port} port`);
 for await (const conn of Deno.listen({ port })) {
@@ -12,8 +21,18 @@ async function handleConn(conn: Deno.Conn) {
     console.log(">", request.url);
 
     const blob = await request.blob();
-    console.log(hexdump(await blob.arrayBuffer()));
+    const buf = new Uint8Array(await blob.arrayBuffer(), 5);
+    console.log(hexdump(buf));
 
-    respondWith(new Response(`responding to ${request.url}`));
+    const req = (Req.decode(buf) as any) as { name: string };
+    console.log(req);
+
+    const res = Res.encode({
+      message: `hello to you, ${req.name || "stanger"}`,
+    }).finish();
+
+    const resp = new Response(res);
+
+    respondWith(resp);
   }
 }
