b72e8b7394bc57784005ac22e32d293557a82c3e
Oleg Prohazko <olegprohazko@gmail.com>
2021-04-22 01:07:11 +0300

    gen dts - init

--- proto.ts
+++ proto.ts
@@ -3,13 +3,14 @@ import type {
   Service,
   Message,
   Method,
+  ReflectionObject
 } from "https://raw.githubusercontent.com/protobufjs/protobuf.js/v6.10.2/index.d.ts";
 
 import protobuf from "./vendor/protobuf@v6.10.2.js";
 
 const lib = protobuf.exports as any;
 
-export { Root, Service, Message, Method };
+export { Root, Service, Message, Method, ReflectionObject };
 
 export function parse(proto: string): { package: string; root: Root } {
   return lib.parse(proto);
