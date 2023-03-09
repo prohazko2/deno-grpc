98232b37f69aad2a12a9e65aa978699c110cd14a
Ranadeep Biswas <mail@rnbguy.at>
2023-03-09 15:31:18 +0100

    similar renaming as past

--- proto.ts
+++ proto.ts
@@ -9,10 +9,10 @@ import type {
   IParserResult
 } from "npm:protobufjs@6.10.2";
 
-import protobuf from "npm:protobufjs@6.10.2";
+import lib from "npm:protobufjs@6.10.2";
 
 export { Root, Type, Field, Service, Message, Method, ReflectionObject };
 
 export function parse(proto: string): IParserResult {
-  return protobuf.parse(proto);
+  return lib.parse(proto);
 }
